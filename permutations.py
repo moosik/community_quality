a34ec20d449900e75450442d477bcc9fb09452ab
moosik <vitalina@git>
2015-12-14 08:34:58 -0800

    permutations now collect the results in a dictionary

--- permutations.py
+++ permutations.py
@@ -8,6 +8,7 @@ import random
 # Performs one round of shuffling, writes shuffled communities to files
 def permute_dics(input_file, shuffle_dir):
     # Create a result_dir
+    # make_sure_path_exists(shuffle_dir)
     make_sure_path_exists(shuffle_dir)
     f = open(input_file, 'r')
     # get the header
@@ -27,40 +28,47 @@ def permute_dics(input_file, shuffle_dir):
 
 ## Loop to create random permutations and process the results for each
 def dir_loop(input_file, shuffle_dir):
-    loop_results = []
+    # Create an empty dictionary to collect the shuffled results
+    all_shuffle = dict()
     for i in range(0, 5):
         random.seed(i)
         permute_dics(input_file, shuffle_dir)
-        loop_results.append(community_quality_extract(shuffle_dir))
-    return loop_results
+        shuffle_res = community_quality_extract(shuffle_dir)
+        for key in shuffle_res.keys():
+            if key not in all_shuffle.keys():
+                all_shuffle.setdefault(key, shuffle_res[key])
+            else:
+                all_shuffle[key].append(shuffle_res[key])
+
+    return all_shuffle
 
 
 #### Transform the obtain results into a dictionary of dictionaries
-def perm_results2files(loop_results):
-    statistics = ["VI", "NMI", "F", "NVD", "RI", "ARI", "JI"]
-    # loop_results[0][0] give the list of pairs of files being compared
-    for i in range(0, len(loop_results[0][0])):
-        # Create an empty dictionary to store the results
-        perm_results = dict()
-        for el in range(len(statistics)):
-            perm_results[statistics[el]] = []
-        # Write the results of all permutations into corresponding lists for each statistic
-        perm_results["VI"].extend([item[1][i] for item in loop_results])
-        perm_results["NMI"].extend([item[2][i] for item in loop_results])
-        perm_results["F"].extend([item[3][i] for item in loop_results])
-        perm_results["NVD"].extend([item[4][i] for item in loop_results])
-        perm_results["RI"].extend([item[5][i] for item in loop_results])
-        perm_results["ARI"].extend([item[6][i] for item in loop_results])
-        perm_results["JI"].extend([item[7][i] for item in loop_results])
-        # Write the dictionary of dictionaries to a file in a wide format
-        # where the first element is the name of the statistic and the rest
-        # are the tab delimited results of the permutations
-        result_file = open(loop_results[0][0][i], "w")
-        for key in perm_results:
-            # convert numbers to strings so they can be written to a file
-            line2write = (str(w) for w in perm_results[key])
-            result_file.write(key + "\t" + "\t".join(line2write) + "\n")
-        result_file.close()
+# def perm_results2files(loop_results):
+#     statistics = ["VI", "NMI", "F", "NVD", "RI", "ARI", "JI"]
+#     # loop_results[0][0] give the list of pairs of files being compared
+#     for i in range(0, len(loop_results[0][0])):
+#         # Create an empty dictionary to store the results
+#         perm_results = dict()
+#         for el in range(len(statistics)):
+#             perm_results[statistics[el]] = []
+#         # Write the results of all permutations into corresponding lists for each statistic
+#         perm_results["VI"].extend([item[1][i] for item in loop_results])
+#         perm_results["NMI"].extend([item[2][i] for item in loop_results])
+#         perm_results["F"].extend([item[3][i] for item in loop_results])
+#         perm_results["NVD"].extend([item[4][i] for item in loop_results])
+#         perm_results["RI"].extend([item[5][i] for item in loop_results])
+#         perm_results["ARI"].extend([item[6][i] for item in loop_results])
+#         perm_results["JI"].extend([item[7][i] for item in loop_results])
+#         # Write the dictionary of dictionaries to a file in a wide format
+#         # where the first element is the name of the statistic and the rest
+#         # are the tab delimited results of the permutations
+#         result_file = open(loop_results[0][0][i], "w")
+#         for key in perm_results:
+#             # convert numbers to strings so they can be written to a file
+#             line2write = (str(w) for w in perm_results[key])
+#             result_file.write(key + "\t" + "\t".join(line2write) + "\n")
+#         result_file.close()
 
 
 
@@ -69,6 +77,12 @@ def perm_results2files(loop_results):
 
 
 temp = dir_loop("data/rosmap_test_train.txt", "shuffle_test1")
-perm_results2files(temp)
+#perm_results2files(temp)
+
+from pushbullet import Pushbullet
+
+pb = Pushbullet('FBwNPNCUvAwiSV2n6cqjfN69Pdei7DjU')
+push = pb.push_note("This is the title", "This is the body")
+
 
 
